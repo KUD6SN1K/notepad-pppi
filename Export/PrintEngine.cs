fd94e241d0c6f5c9a1416f41f0a980d1cea5aa24
KUD6SN1K <137761765+KUD6SN1K@users.noreply.github.com>
2026-04-12 13:05:12 +0300

    Create PrintEngine.cs

---
+++ Export/PrintEngine.cs
@@ -0,0 +1,14 @@
+// ============================================
+// MODULE 7: Export and Print
+// Part of Group 3: File, Export, Settings, Help
+// ============================================
+
+// TODO: Print current document
+// TODO: Show print preview
+
+public class PrintEngine
+{
+    public void PrintDocument() { }
+    public void ShowPrintPreview() { }
+    public void ConfigurePageSetup() { }
+}
