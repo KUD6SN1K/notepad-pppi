93e51918c24d04ae4c5e7d465151fea123b6b653
KUD6SN1K <137761765+KUD6SN1K@users.noreply.github.com>
2026-04-12 13:04:58 +0300

    Create FileManager.cs

---
+++ Files/FileManager.cs
@@ -0,0 +1,29 @@
+// ============================================
+// MODULE 4: File Management
+// Part of Group 3: File, Export, Settings, Help
+// ============================================
+
+// TODO: Open files with encoding support (UTF-8, ANSI)
+// TODO: Save files with selected encoding
+// TODO: Track recent files (last 10)
+
+public class FileManager
+{
+    // Opens file from disk
+    public string OpenFile(string filePath)
+    {
+        return "";
+    }
+
+    // Saves file to disk
+    public void SaveFile(string filePath, string content, string encoding) { }
+
+    // Gets list of recent files
+    public string[] GetRecentFiles()
+    {
+        return new string[] { };
+    }
+
+    // Enables autosave
+    public void EnableAutosave(int intervalMinutes) { }
+}
