7f4dd713889b6fff7e4be649c447efaf89ef629a
KUD6SN1K <137761765+KUD6SN1K@users.noreply.github.com>
2026-04-12 13:03:15 +0300

    Create Autocomplete.cs

---
+++ Semantics/Autocomplete.cs
@@ -0,0 +1,29 @@
+// ============================================
+// MODULE 2: Language Semantics - Autocomplete
+// Part of Group 2: Semantics, Search, History
+// ============================================
+
+// TODO: Show autocomplete suggestions on Ctrl+Space
+// TODO: Suggest keywords based on current language
+// TODO: Suggest variable names from current document
+
+public class Autocomplete
+{
+    // Shows autocomplete dropdown
+    public void ShowSuggestions(string currentWord)
+    {
+        // Opens dropdown near cursor
+    }
+
+    // Gets keywords for current language
+    public string[] GetKeywordsForLanguage(string language)
+    {
+        return new string[] { };
+    }
+
+    // Inserts selected suggestion
+    public void InsertSuggestion(string suggestion)
+    {
+        // Replaces current word with suggestion
+    }
+}
