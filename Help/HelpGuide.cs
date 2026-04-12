6a3f28dbb1513d8ea52dac1248c6ec241963ffec
KUD6SN1K <137761765+KUD6SN1K@users.noreply.github.com>
2026-04-12 13:06:43 +0300

    Create HelpGuide.cs

---
+++ Help/HelpGuide.cs
@@ -0,0 +1,13 @@
+// ============================================
+// MODULE 10: Help Guide
+// Part of Group 3: File, Export, Settings, Help
+// ============================================
+
+// TODO: Open help on F1, show hotkeys
+
+public class HelpGuide
+{
+    public void OpenHelp() { }
+    public string[] GetHotkeysList() { return new string[] { }; }
+    public void ShowAbout() { }
+}
